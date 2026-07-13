bb8955d157d42cea2413b6e5603985a39ac8071b
PYDI VINAY <vinaypydi6@gmail.com>
2026-07-13 20:14:20 +0530

    Update Main.java

---
+++ src/com/cabbooking/ui/Main.java
@@ -0,0 +1,13 @@
+package com.cabbooking.ui;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) { }
+
+        SwingUtilities.invokeLater(() -> new LoginFrame().setVisible(true));
+    }
+}
